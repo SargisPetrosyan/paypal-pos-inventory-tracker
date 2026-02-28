0441271b63da621bb291ec669db34bbcee6f914e
Sargis <saqomax@gmail.com>
2026-02-28 14:03:36 +0100

    add timestamp and shop id inde

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ import uuid
 
 class InventoryBalanceUpdateModel(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    timestamp:datetime.datetime = Field(default_factory=DateTime)
-    shop_id:uuid.UUID = Field(default_factory=uuid.uuid4)
+    timestamp:datetime.datetime = Field(default_factory=DateTime,index=True)
+    shop_id:uuid.UUID = Field(default_factory=uuid.uuid4,index=True)
     product_id:uuid.UUID  = Field(default_factory=uuid.uuid1)
     variant_id:uuid.UUID = Field(default_factory=uuid.uuid1)
     before:int
